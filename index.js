ea29889d87b0784cf9f597acc8b6b0db2a9875d6
nafissa <atlaghnafissa@iav.ac.ma>
2022-11-30 19:20:57 +0100

    now it ll work defffffffffffurgh

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ import knexPostgis from 'knex-postgis'
 import knex from 'knex'
 import clinics from './DataClinics.json' assert { type: "json" };
 
-
-const pool = new pg.Pool();
+const connectionString =
+  "postgresql://postgres:BLsWHcahT5ZglrAxHSvH@containers-us-west-84.railway.app:6529/railway";
+const pool = new pg.Pool({
+    connectionString,
+});
 const app = express()
 const port = process.env.PORT || 5000
 pool.connect()
@@ -28,8 +31,8 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/db', async (req, res)=>{
-    var {result} = await pool.query('SELECT * FROM clinics')
-    res.json(result)
+    var result = await pool.query('SELECT * FROM clinics;')
+    res.json({result})
 })
 
 // app.post('/add_element', (req, res)=>{
