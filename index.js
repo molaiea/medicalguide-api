e29853c82a2d9e46e67648d7dad543263c764a9f
nafissa <atlaghnafissa@iav.ac.ma>
2022-11-30 01:42:53 +0100

    knex test

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import express from 'express'
 import pg from "pg";
+import knexPostgis from 'knex-postgis'
+import knex from 'knex'
+
 
 const pool = new pg.Pool();
 const app = express()
 const port = process.env.PORT || 5000
-
+const db = knex('pg')
+const st = knexPostgis(db)
 app.get('/', (req, res)=>{
     console.log(`${req} is asking for connection`)
     res.send("success")
 })
 
 app.get('/db', async (req, res)=>{
-    const {rows} = await pool.query('SELECT * FROM clinics')
+    const {rows} = await db('clinics').withSchema('public').select('*').where('id', 1)
     res.send(rows)
 })
 app.listen(port, ()=>{
