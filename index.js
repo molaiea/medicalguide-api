79ac7601ea239a981fdd0473ee27be5283d29611
nafissa <atlaghnafissa@iav.ac.ma>
2022-11-30 19:39:35 +0100

    hm

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 import express from 'express'
 import pg from "pg";
-import knexPostgis from 'knex-postgis'
-import knex from 'knex'
+// import knexPostgis from 'knex-postgis'
+// import knex from 'knex'
 import clinics from './DataClinics.json' assert { type: "json" };
 
 const connectionString =
   "postgresql://postgres:BLsWHcahT5ZglrAxHSvH@containers-us-west-84.railway.app:6529/railway";
-// const pool = new pg.Pool({
-//     connectionString,
-// });
+const pool = new pg.Pool({
+    connectionString,
+});
 const app = express()
 const port = process.env.PORT || 5000
 const myclinics = clinics.features
-const mypg = knex({
-    client: 'pg',
-    connection: connectionString,
-  });
+// const mypg = knex({
+//     client: 'pg',
+//     connection: connectionString,
+//   });
   
 // const mydb = knex({
 //     client:'pg',
@@ -34,8 +34,8 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/db', async (req, res)=>{
-    //var result = await pool.query('SELECT * FROM clinics;')
-    res.json(mypg('clinics').withSchema('public').select("*"))
+    var result = await pool.query('SELECT * FROM clinics;')
+    res.json({result})
 })
 
 // app.post('/add_element', (req, res)=>{
