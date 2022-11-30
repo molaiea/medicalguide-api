d46381adcf8305a8b3a44816a1425e9e37c71fb8
nafissa <atlaghnafissa@iav.ac.ma>
2022-11-30 01:34:50 +0100

    adding db

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
+import pg from "pg";
 
+const pool = new pg.Pool();
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -8,6 +10,10 @@ app.get('/', (req, res)=>{
     res.send("success")
 })
 
+app.get('/db', async (req, res)=>{
+    const {rows} = await pool.query('SELECT * FROM clinics')
+    res.send(rows)
+})
 app.listen(port, ()=>{
-    console.log("listening on port")
+    console.log(`listening on port ${port}`)
 })
